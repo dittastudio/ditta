0cd7af0949ec8751dedf4e186bd1c5127049968c
Michael Pumo <hello@michaelpumo.com>
2024-06-20 23:41:27 +0100

    Team bento.

--- components/bento/Team.vue
+++ components/bento/Team.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { BentoTeamStoryblok } from '@/types/storyblok'
+import IconSign from '@/assets/icons/sign.svg'
 
 interface Props {
   block: BentoTeamStoryblok
@@ -11,8 +12,31 @@ const { block } = defineProps<Props>()
 <template>
   <div
     v-editable="block"
-    class="w-full h-full bg-grey"
+    class="relative w-full h-full bg-gradient-to-t from-offblack/25 to-grey p-20"
   >
-    <!-- <pre>{{ block.component }}</pre> -->
+    <IconSign class="sign absolute -top-8 left-[calc(50%_-_35px)]" />
+
+    <img
+      class="us w-11/12 h-auto block absolute bottom-0 left-1/2 -translate-x-1/2"
+      src="/images/us.png"
+      alt="ditta"
+    >
   </div>
 </template>
+
+<style lang="postcss" scoped>
+@keyframes swing {
+  0% { transform: rotate(5deg); }
+  100% { transform: rotate(-5deg); }
+}
+
+.sign {
+  animation: swing ease-in-out 1s infinite alternate;
+  transform-origin: center top;
+  filter: drop-shadow(4px 4px 2px rgba(0, 0, 0, 0.1));
+}
+
+.us {
+  filter: drop-shadow(15px 15px 1px rgba(0, 0, 0, 0.1));
+}
+</style>
