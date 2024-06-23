f5c9c991a66c1f25d05a7644109d31b697b74ecc
Michael Pumo <hello@michaelpumo.com>
2024-06-23 23:54:16 +0100

    insta

--- components/bento/Instagram.vue
+++ components/bento/Instagram.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 import type { BentoInstagramStoryblok } from '@/types/storyblok'
+import IconDitta from '@/assets/icons/ditta.svg'
+import IconInstagram from '@/assets/icons/instagram.svg'
 
 interface Props {
   block: BentoInstagramStoryblok
@@ -11,8 +13,34 @@ const { block } = defineProps<Props>()
 <template>
   <div
     v-editable="block"
-    class="w-full h-full bg-grey"
+    class="relative w-full h-full bg-grey"
   >
-    <!-- <pre>{{ block.component }}</pre> -->
+    <MediaImage
+      v-if="block.image"
+      class="absolute z-0 pointer-events-none [&>img]:object-cover [&>img]:!h-full"
+      :asset="block.image"
+      sizes="
+        100vw
+        sm:100vw
+      "
+    />
+
+    <IconInstagram class="absolute top-20 right-20 z-10 w-20 h-20 text-white" />
+
+    <div class="absolute left-0 bottom-0 z-10 w-full flex items-center justify-start gap-10 p-20 bg-gradient-to-t from-black/50 to-black/0">
+      <div class="rounded-full bg-pink w-40 h-40 p-8">
+        <IconDitta class="size-full text-offblack block" />
+      </div>
+
+      <div class="flex flex-col gap-[2px]">
+        <h2 class="text-12 text-white font-bold">
+          _dittastudio
+        </h2>
+
+        <p class="text-12 text-white">
+          {{ block.caption }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
