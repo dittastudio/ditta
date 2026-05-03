440b273dbf461ef1d6b22e059bd8fdf079770de3
Michael Pumo <hello@michaelpumo.com>
2026-05-03 21:06:33 +0100

    fix: adjust padding and margin for Hero component logo

--- app/components/block/Hero.vue
+++ app/components/block/Hero.vue
@@ -3,7 +3,7 @@ import IconLogo from '@/assets/icons/ditta.svg'
 </script>
 
 <template>
-  <div class="wrapper py-20 min-h-screen flex flex-col justify-end">
-    <IconLogo class="w-full h-auto" />
+  <div class="wrapper min-h-screen flex flex-col justify-end">
+    <IconLogo class="w-full h-auto -mb-1.5" />
   </div>
 </template>
