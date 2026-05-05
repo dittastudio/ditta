61bfa4b8d48aa8fc2f9017714b563b9b96869d09
Michael Pumo <hello@michaelpumo.com>
2026-05-05 14:29:51 +0100

    fix: adjust padding in Footer component for consistent layout

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -10,7 +10,7 @@ const { text } = defineProps<Props>()
 
 <template>
   <footer class="bg-black text-grey min-h-screen flex">
-    <div class="wrapper w-full py-10 flex flex-col justify-between gap-6">
+    <div class="wrapper w-full pt-(--app-vertical-rhythm) pb-10 flex flex-col justify-between gap-6">
       <div
         v-if="storyblokRichTextContent(text)"
         :class="[
