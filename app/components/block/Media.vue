18910d2f9ea57a70ab27130b66e87b95abfb5459
Will The Dev <hello@willthe.dev>
2026-05-05 16:53:54 +0100

    fix: redone image grid

--- app/components/block/Media.vue
+++ app/components/block/Media.vue
@@ -9,68 +9,62 @@ const { block } = defineProps<Props>()
 
 // const media = computed(() => block.media?.[0])
 
-const setSizes = computed(() => {
-  switch (block.placement) {
-    case 'inset':
-      return `
-        xs:100vw
-        sm:100vw
-        md:80vw
-        lg:80vw
-        xl:1147px
-      `
-    case 'wide':
-      return `
-        xs:100vw
-        sm:100vw
-        md:100vw
-        lg:100vw
-        xl:1380px
-      `
-    default:
-      return `
-        xs:100vw
-        sm:100vw
-        md:100vw
-        lg:100vw
-        xl:100vw
-      `
-  }
-})
+// const setSizes = computed(() => {
+//   switch (block.layout) {
+//     case 'inset':
+//       return `
+//         xs:100vw
+//         sm:100vw
+//         md:80vw
+//         lg:80vw
+//         xl:1147px
+//       `
+//     case 'wide':
+//       return `
+//         xs:100vw
+//         sm:100vw
+//         md:100vw
+//         lg:100vw
+//         xl:1380px
+//       `
+//     default:
+//       return `
+//         xs:100vw
+//         sm:100vw
+//         md:100vw
+//         lg:100vw
+//         xl:100vw
+//       `
+//   }
+// })
 </script>
 
 <template>
   <div
     v-editable="block"
     :class="{
-      'wrapper grid gap-x-(--app-gutter-inner) grid-cols-12': block.placement !== 'full',
-      'pt-(--app-gutter-inner)': block.spacing_top_media && block.spacing_top,
-      'pt-(--app-vertical-rhythm)': block.spacing_top && !block.spacing_top_media,
-      'pb-(--app-gutter-inner)': block.spacing_bottom_media && block.spacing_bottom,
-      'pb-(--app-vertical-rhythm)': block.spacing_bottom && !block.spacing_bottom_media,
+      'pt-(--app-vertical-rhythm)': block.spacing_top,
+      'pb-(--app-vertical-rhythm)': block.spacing_bottom,
     }"
+    class="overflow-clip"
   >
-    <div
-      class="flex flex-col md:flex-row gap-(--app-gutter-inner)"
-      :class="{
-        'col-span-full': block.placement === 'wide',
-        'col-span-full md:col-start-2 md:col-span-10': block.placement === 'inset',
-        'pt-(--app-vertical-spacing)': block.placement === 'full',
-      }"
-    >
+    <div class="wrapper grid grid-cols-1 md:grid-cols-2 gap-(--app-gutter-inner) grid-flow-dense">
       <div
-        class="basis-1/2 grow"
         v-for="media in block.media"
         :key="media._uid"
+        :class="{
+          'col-span-full': media.layout === 'full' || media.layout === 'wide',
+          'w-dvw mx-[calc(50%-50dvw)] max-w-none': media.layout === 'full',
+          'col-span-1': media.layout === 'half',
+        }"
       >
         <NuxtImg
-          v-if="media && media.image?.filename && storyblokAssetType(media.image.filename) === 'image'"
-          class="block"
+          v-if="media.image && media.image.filename && storyblokAssetType(media.image.filename) === 'image'"
+          class="block w-full"
           :src="media.image.filename"
           :alt="media.image.alt || ''"
           :width="storyblokImageDimensions(media.image.filename).width"
           :height="storyblokImageDimensions(media.image.filename).height"
-          :sizes="setSizes"
           loading="lazy"
         />
       </div>
