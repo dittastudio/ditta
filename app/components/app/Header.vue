80b7e1084dbc195696419312d984845db55e656e
Will The Dev <hello@willthe.dev>
2026-05-12 14:08:29 +0100

    fix: minor fix to classes in header

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -10,13 +10,18 @@ const textClass = computed(() => (headerTheme.value === 'dark' ? 'text-grey' : '
 </script>
 
 <template>
-  <header :class="['absolute top-0 z-10 w-full transition-colors duration-1000 ease-out', textClass]">
+  <header
+    class="absolute top-0 z-10 w-full transition-colors duration-1000 ease-out"
+    :class="textClass"
+  >
     <div class="wrapper flex justify-between items-start pt-(--app-gutter-outer) md:pt-20">
       <p
         v-if="tagline"
         class="max-lg:hidden text-navigation"
       >
-        {{ tagline }}
+        <NuxtLink to="/">
+          {{ tagline }}
+        </NuxtLink>
       </p>
 
       <slot />
