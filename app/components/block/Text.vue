de3bce80a39a29e644dc3e15bb7d3a413e9a6b2a
Michael Pumo <hello@michaelpumo.com>
2026-05-12 22:59:32 +0100

    fix: remove unnecessary margin class for right-aligned block in Text component

--- app/components/block/Text.vue
+++ app/components/block/Text.vue
@@ -21,7 +21,7 @@ const { block } = defineProps<Props>()
       v-if="storyblokRichTextContent(block.text)"
       class="col-span-1 prose prose-h2:text-body prose-h2:font-medium prose-p:text-body text-pretty max-w-[60ch]"
       :class="{
-        'md:ml-auto lg:col-start-2': block.align_right,
+        'lg:col-start-2': block.align_right,
       }"
     >
       <StoryblokText :html="block.text" />
