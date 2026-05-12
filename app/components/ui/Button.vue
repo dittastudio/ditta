2c2402735c57509c65bfea4001076b2cbf2116e3
Will The Dev <hello@willthe.dev>
2026-05-12 11:03:37 +0100

    feat: arrow animation on button, seems overkill

--- app/components/ui/Button.vue
+++ app/components/ui/Button.vue
@@ -59,7 +59,6 @@ onUnmounted(() => {
       v-if="text"
       class="absolute mt-[-0.15em] mb-[-0.1em]"
     >
-      <!-- {{ text }} -->
       <UiTextMorph
         :text="text"
         :auto-animate="false"
@@ -67,30 +66,87 @@ onUnmounted(() => {
       />
     </span>
 
-    <span class="ui-button__icon size-[1em] rounded-full aspect-square bg-accent flex items-center justify-center">
-      <IconPixelArrow class="arrow w-5 rotate-45" />
+    <span
+      class="ui-button__icon size-[1em] rounded-full aspect-square bg-accent flex items-center justify-center overflow-hidden"
+    >
+      <IconPixelArrow class="ui-button__arrow w-[55%] rotate-45" />
     </span>
   </span>
 </template>
 
-<style scoped>
+<style>
 @reference "@/assets/css/app.css";
 
 .ui-button {
   font-size: utopia.clamp(16, 28);
 }
 
+.ui-button__bg {
+  scale: 1 1;
+  transform: translateZ(0);
+  transition: scale 0.8s var(--ease-spring-3);
+
+  a:hover & {
+    transition: scale 0.4s var(--ease-out);
+    scale: var(--scale-x) var(--scale-y);
+  }
+}
+
 .ui-button__icon {
   font-size: utopia.clamp(30, 40);
 }
 
-.ui-button__bg {
-  scale: 1 1;
-  transition: scale 0.8s var(--ease-spring-3);
+.ui-button__arrow {
+  a:hover & {
+    animation: arrow-fire 0.8s both;
+  }
+}
 
-  .ui-button:hover & {
-    transition: scale 0.4s var(--ease-out);
-    scale: var(--scale-x) var(--scale-y);
+/* Annoyingly easing vars don't work in keyframes */
+@keyframes arrow-fire {
+  0% {
+    translate: 0 0;
+    scale: 1 1;
+    filter: blur(0px);
+    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+  12% {
+    translate: -30% 30%;
+    scale: 0.7 1.1;
+    filter: blur(1px);
+    animation-timing-function: cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+  44% {
+    translate: 260% -260%;
+    scale: 1 1;
+    filter: blur(1px);
+    animation-timing-function: none;
+  }
+  45% {
+    translate: -260% 260%;
+    scale: 1 1;
+    filter: blur(1px);
+    animation-timing-function: linear(
+      0,
+      0.006,
+      0.025 2.8%,
+      0.101 6.1%,
+      0.539 18.9%,
+      0.721 25.3%,
+      0.849 31.5%,
+      0.937 38.1%,
+      0.968 41.8%,
+      0.991 45.7%,
+      1.006 50.1%,
+      1.015 55%,
+      1.017 63.9%,
+      1.001
+    );
+  }
+  100% {
+    translate: 0 0;
+    scale: 1 1;
+    filter: blur(0px);
   }
 }
 </style>
