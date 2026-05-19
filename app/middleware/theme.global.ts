4a70e577266a0ccfc3f331f938b9255f4731bffb
Will The Dev <hello@willthe.dev>
2026-05-19 18:38:00 +0100

    fix: theme on reload fix

--- app/middleware/theme.global.ts
+++ app/middleware/theme.global.ts
@@ -3,20 +3,22 @@ import type { Themes } from '@/types/app'
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const theme = useTheme()
+  const scrollTheme = useBlockTheme()
 
   try {
     const story = await useStory<Page | Project>(to.path, {}, {}, {}, to.query)
     const first = story.value.content.blocks?.[0]
 
     if (first && 'theme' in first) {
-      const newStrapline = first.theme as Themes
+      const firstTheme = first.theme as Themes
 
       if (import.meta.server) {
-        theme.value = { ...theme.value, strapline: newStrapline }
+        theme.value = { ...theme.value, strapline: firstTheme }
+        scrollTheme.value = firstTheme
       } else {
         const router = useRouter()
         const stop = router.afterEach(() => {
-          theme.value = { ...theme.value, strapline: newStrapline }
+          theme.value = { ...theme.value, strapline: firstTheme }
           stop()
         })
       }
