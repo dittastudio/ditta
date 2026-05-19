98f54c5f1bed014dfff1de7364f085b3c9a2aa9a
Will The Dev <hello@willthe.dev>
2026-05-19 18:09:50 +0100

    fix: theme converted to radio buttons

--- app/components/app/Theme.vue
+++ app/components/app/Theme.vue
@@ -25,24 +25,34 @@ const theme = useTheme()
 </script>
 
 <template>
-  <ul class="flex gap-2 items-center justify-center">
-    <li
-      v-for="option in options"
-      :key="option.value"
-    >
-      <button
-        type="button"
-        @click="theme = { ...theme, accent: option.value }"
+  <fieldset class="contents">
+    <legend class="sr-only">Accent colour</legend>
+
+    <ul class="flex gap-2 items-center justify-center">
+      <li
+        v-for="option in options"
+        :key="option.value"
       >
-        <span class="sr-only">{{ option.label }}</span>
-        <span
-          class="block size-5 rounded-full outline-2 outline-offset-2 transition-colors duration-300 ease-outCubic"
-          :class="[
-            bgClass[option.value],
-            theme.accent === option.value ? outlineClass[option.value] : 'outline-transparent',
-          ]"
-        />
-      </button>
-    </li>
-  </ul>
+        <label>
+          <span class="sr-only">{{ option.label }}</span>
+
+          <input
+            type="radio"
+            name="accent"
+            class="sr-only"
+            :value="option.value"
+            v-model="theme.accent"
+          />
+
+          <span
+            class="block size-5 rounded-full outline-2 outline-offset-2 transition-colors duration-300 ease-outCubic"
+            :class="[
+              bgClass[option.value],
+              theme.accent === option.value ? outlineClass[option.value] : 'outline-transparent',
+            ]"
+          />
+        </label>
+      </li>
+    </ul>
+  </fieldset>
 </template>
