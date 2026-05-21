389fca74db08052f5f0c58478881b5df88311094
Michael Pumo <hello@michaelpumo.com>
2026-05-21 20:09:24 +0100

    fix: wrap accent options in ClientOnly component

--- app/components/app/Accent.vue
+++ app/components/app/Accent.vue
@@ -26,31 +26,33 @@ const appStore = useAppStore()
   <fieldset class="contents">
     <legend class="sr-only">Accent colour</legend>
 
-    <ul class="flex gap-2 items-center justify-center">
-      <li
-        v-for="option in options"
-        :key="option.value"
-      >
-        <label>
-          <span class="sr-only">{{ option.label }}</span>
+    <ClientOnly>
+      <ul class="flex gap-2 items-center justify-center">
+        <li
+          v-for="option in options"
+          :key="option.value"
+        >
+          <label>
+            <span class="sr-only">{{ option.label }}</span>
 
-          <input
-            type="radio"
-            name="accent"
-            class="sr-only"
-            :value="option.value"
-            v-model="appStore.accent"
-          />
+            <input
+              type="radio"
+              name="accent"
+              class="sr-only"
+              :value="option.value"
+              v-model="appStore.accent"
+            />
 
-          <span
-            class="block size-5 rounded-full outline-2 outline-offset-2 transition-colors duration-300 ease-outCubic"
-            :class="[
-              bgClass[option.value],
-              appStore.accent === option.value ? outlineClass[option.value] : 'outline-transparent',
-            ]"
-          />
-        </label>
-      </li>
-    </ul>
+            <span
+              class="block size-5 rounded-full outline-2 outline-offset-2 transition-colors duration-300 ease-outCubic"
+              :class="[
+                bgClass[option.value],
+                appStore.accent === option.value ? outlineClass[option.value] : 'outline-transparent',
+              ]"
+            />
+          </label>
+        </li>
+      </ul>
+    </ClientOnly>
   </fieldset>
 </template>
