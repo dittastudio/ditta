296a587657f3f332c532f7b0fce376606a0c41c8
Michael Pumo <hello@michaelpumo.com>
2026-07-09 19:25:13 +0100

    feat: add @unhead/schema-org/vue to dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -114,6 +114,7 @@ export default defineNuxtConfig({
         '@plausible-analytics/tracker',
         '@storyblok/vue',
         '@tiptap/core',
+        '@unhead/schema-org/vue',
         '@vue/devtools-core',
         '@vue/devtools-kit',
         '@vueuse/core',
