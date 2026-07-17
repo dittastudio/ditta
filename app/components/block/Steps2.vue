eb84f4b91203989a934c303e5529634971598e19
Michael Pumo <hello@michaelpumo.com>
2026-07-17 15:30:26 +0100

    feat: enhance Steps2 component with image display and improved scroll animations

--- app/components/block/Steps2.vue
+++ app/components/block/Steps2.vue
@@ -11,19 +11,22 @@ interface Props {
 }
 
 const { block } = defineProps<Props>()
+
 const wrapperRef = useTemplateRef('wrapper')
 const listRef = useTemplateRef('list')
+const imagesRef = useTemplateRef('images')
 
 const count = computed(() => block.steps?.length ?? 0)
 
-let scrollTrigger: ScrollTrigger | undefined
+let listTrigger: ScrollTrigger | undefined
+let imagesTrigger: ScrollTrigger | undefined
 
 onMounted(async () => {
   await nextTick()
 
   if (count.value < 2) return
 
-  const tween = gsap.fromTo(
+  const listTween = gsap.fromTo(
     listRef.value,
     {
       yPercent: 0,
@@ -40,12 +43,36 @@ onMounted(async () => {
     },
   )
 
-  scrollTrigger = tween.scrollTrigger
+  listTrigger = listTween.scrollTrigger
+
+  const imagesTween = gsap.fromTo(
+    imagesRef.value,
+    {
+      yPercent: 0,
+    },
+    {
+      scrollTrigger: {
+        trigger: wrapperRef.value,
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: 1,
+      },
+      ease: 'none',
+      yPercent: -((count.value - 1) / count.value) * 100,
+    },
+  )
+
+  imagesTrigger = imagesTween.scrollTrigger
 })
 
 onUnmounted(() => {
-  scrollTrigger?.kill()
-  scrollTrigger = undefined
+  ;[listTrigger, imagesTrigger].forEach((trigger) => {
+    if (trigger) {
+      trigger.kill()
+      trigger = undefined
+    }
+  })
+
   gsap.killTweensOf(listRef.value)
 })
 </script>
@@ -54,17 +81,17 @@ onUnmounted(() => {
   <div
     v-editable="block"
     ref="wrapper"
-    class="wrapper bg-linear-to-b from-mood to-olive"
+    class="wrapper"
     :style="{
       '--inset-top': block.spacing_top ? 'calc(var(--app-vertical-rhythm) / 2)' : '0px',
       '--inset-bottom': block.spacing_bottom ? 'calc(var(--app-vertical-rhythm) / 2)' : '0px',
       paddingTop: 'var(--inset-top)',
       paddingBottom: 'var(--inset-bottom)',
-      height: `calc(100lvh * ${count})`,
+      height: `calc(200lvh * ${count})`,
     }"
   >
     <div
-      class="sticky rounded-20 bg-[salmon] overflow-hidden"
+      class="sticky rounded-20 bg-[salmon] overflow-hidden grid grid-cols-2"
       :style="{
         top: 'var(--inset-top)',
         height: 'calc(100lvh - var(--inset-top) - var(--inset-bottom))',
@@ -72,16 +99,16 @@ onUnmounted(() => {
     >
       <ul
         ref="list"
-        class="w-full bg-[green] grid grid-cols-1"
-        :style="{
-          height: `calc((100lvh - var(--inset-top) - var(--inset-bottom)) * ${count})`,
-        }"
+        class="w-full"
       >
         <li
           v-for="(step, index) in block.steps"
           v-editable="step"
           :key="step._uid"
           class="w-full"
+          :style="{
+            height: `calc((100lvh - var(--inset-top) - var(--inset-bottom))`,
+          }"
         >
           <CardStep
             :number="`0${index + 1}`.slice(-2)"
@@ -91,6 +118,23 @@ onUnmounted(() => {
             :theme="'theme' in step ? (step.theme as Themes) : undefined"
             class="rounded-none! h-full!"
           />
+        </li>
+      </ul>
+
+      <ul
+        ref="images"
+        class="w-full"
+      >
+        <li
+          v-for="(step, index) in block.steps"
+          v-editable="step"
+          :key="step._uid"
+          class="w-full"
+          :style="{
+            height: `calc((100lvh - var(--inset-top) - var(--inset-bottom))`,
+          }"
+        >
+          IMAGE {{ index }} WILL GO HERE
         </li>
       </ul>
     </div>
