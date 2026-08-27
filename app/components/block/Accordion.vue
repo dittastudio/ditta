d5942e15413215eece9684b494f908bf62bb488a
Michael Pumo <hello@michaelpumo.com>
2026-08-27 21:51:07 +0100

    feat: introductory text

--- app/components/block/Accordion.vue
+++ app/components/block/Accordion.vue
@@ -17,11 +17,23 @@ const { block } = defineProps<Props>()
       'pb-(--app-vertical-rhythm)': block.spacing_bottom,
     }"
   >
-    <div class="w-full max-w-174">
-      <UiLockup
-        :heading="block.heading"
-        :copy="block.copy"
-      />
+    <div class="w-full max-w-174 flex flex-col gap-20">
+      <div
+        v-if="block.heading || storyblokRichTextContent(block.copy)"
+        class="prose prose-p:text-pretty prose-p:text-lead text-center"
+      >
+        <h2
+          v-if="block.heading"
+          class="text-title whitespace-pre-wrap trim-both"
+        >
+          {{ block.heading }}
+        </h2>
+
+        <StoryblokText
+          v-if="storyblokRichTextContent(block.copy)"
+          :html="block.copy"
+        />
+      </div>
 
       <div class="w-full grid gap-1 rounded-20 overflow-hidden">
         <template
