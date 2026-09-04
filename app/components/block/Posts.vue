d905e530113a8e8af1f5457508ab4d72677c1f36
Michael Pumo <hello@michaelpumo.com>
2026-09-04 15:11:40 +0100

    feat: set up posts block

---
+++ app/components/block/Posts.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import type { BlockPosts } from '#storyblok-components'
+
+interface Props {
+  block: BlockPosts
+}
+
+const { block } = defineProps<Props>()
+</script>
+
+<template>
+  <div
+    v-editable="block"
+    class="wrapper flex flex-col justify-center"
+    :class="{
+      'pt-(--app-vertical-rhythm)': block.spacing_top,
+      'pb-(--app-vertical-rhythm)': block.spacing_bottom,
+    }"
+  >
+    <pre>{{ block }}</pre>
+  </div>
+</template>
